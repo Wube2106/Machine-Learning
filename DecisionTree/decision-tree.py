8d05c768f843c60a6536f6e0534fe858e28a3ac6
Wubishet Yitayew <wubishetyitayew2106@gmail.com>
2026-03-13 23:56:59 +0300

    Add info_gain,build_tree and predict function in the decision tree

--- DecisionTree/decision-tree.py
+++ DecisionTree/decision-tree.py
@@ -1,5 +1,5 @@
 #for each feature, find entorpy , find gain inforamtion 
-#compare the ig of features and select the feature which has lowest ig
+#compare the ig of features and select the feature which has highest ig
 #then devide the dataset based on the selected feature
 #recurisivel do thie task until agiven criteria is met.
 import math
@@ -27,7 +27,8 @@ class DecisionTree():
     total=len(rows)
     for count in label_count.values():
       p=count/total
-      ent-=p*math.log2(p)
+      if p>0:
+        ent-=p*math.log2(p)
     return ent
     
   def split_data(self,rows,feature):
@@ -42,23 +43,64 @@ class DecisionTree():
   def info_gain(self,rows,feature,target):
     splits=self.split_data(rows,feature)
     total_entropy=self.entropy(rows,target)
-    weighted_entorpy=0
+    weighted_entropy=0
     for subset in splits.values():
       p=len(subset)/len(rows)
-      weighted_entorpy+=p*self.entropy(subset,target)
-    ig=total_entropy-weighted_entorpy
+      weighted_entropy+=p*self.entropy(subset,target)
+    ig=total_entropy-weighted_entropy
     
     return ig
   
+  def build_tree(self,rows,features,target):
+    labels=[row[target] for row in rows]
+    if labels.count(labels[0])==len(labels):
+      return labels[0]
+    if not features:
+      return max(set(labels),key=labels.count)
+    
+    best=max(features,key=lambda f: self.info_gain(rows,f,target))
+    tree={best:{}}
+    remaining_features=[f for f in features if f !=best]
+    
+    for value,subset in self.split_data(rows,best).items():
+      tree[best][value]=self.build_tree(subset,remaining_features,target)
+    return tree
 
+  def predict(self,tree,sample,default=None):
+    if not isinstance(tree,dict):
+      return tree
+    
+    feature=next(iter(tree))
+    value_sample=sample.get(feature)
 
-
+    sub_tree=tree[feature].get(value_sample)
+    if sub_tree is None:
+      return default
+    
+    return self.predict(sub_tree,sample,default)
     
 entro=DecisionTree()
-result=entro.entropy(data,target)
+entropy_result=entro.entropy(data,target)
 splitted_data=entro.split_data(data,"Outlook")
-info_gain=entro.info_gain(data,"Outlook",target)
-print(result)
+information_gain=entro.info_gain(data,"Outlook",target)
+
+tree=entro.build_tree(data,features,target)
+labels=[row[target] for row in data]
+majority_class=max(set(labels),key=labels.count)
+
+sample = {
+    "Outlook": "Sunny",
+    "Temperature": "Hot",
+    "Humidity": "High"
+}
+
+
+prediction=entro.predict(tree,sample,majority_class)
+print(entropy_result)
 print(splitted_data)
-print(len(splitted_data))
-print(info_gain)
+print(information_gain)
+print(tree)
+print(prediction)
+
+
+
